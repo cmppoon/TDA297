a5cd6bcf1e838b2ec0afb627b70c7c5e165d452e
Chaiyapruek Muangsiri <cmp.poon@gmail.com>
2019-02-18 22:29:36 +0100

    basic broadcast for sequencer working now

--- ReliableOrderedCaster.java
+++ ReliableOrderedCaster.java
@@ -1,6 +1,8 @@
 
 import mcgui.*;
+import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Queue;
 import java.util.LinkedList;
@@ -10,28 +12,59 @@ public class ReliableOrderedCaster extends Multicaster {
     /**
      * No initializations needed for this simple one
      */
-    private int sequencerId = 0;
+    private int sequencerId;
     //history list to keep track of messages since last broadcast
     private List<MyMessage> historyList = new ArrayList<MyMessage>(); 
-    private int localSequence = 0;
+    private int localSequence;
     private Queue<MyMessage> holdBackQueue = new LinkedList<MyMessage>();
+    private int[] activeHost;
+    //add list of all active hosts
 
     public void init() {
+        activeHost = new int[hosts];
+        for(int i = 0; i<hosts; i++){
+            activeHost[i] = 1;
+        }
+        sequencerId = getMinActiveHost();
+        localSequence = 0;
+        mcui.debug("Sequencer is "+ sequencerId);
         mcui.debug("The network has "+hosts+" hosts!");
+    }
+
+    public int getMinActiveHost() {
+        for(int i=0; i<hosts;i++){
+            if (activeHost[i] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void sequencerCast(String messagetext, int messageCreator){
+        for(int i=0; i < hosts; i++) {
+            /* Sends to everyone except itself */
+            if(i != id) {
+                bcom.basicsend(i,new MyMessage(id, messagetext, messageCreator, localSequence));
+            }
+        }
+        localSequence++;
     }
         
     /**
      * The GUI calls this module to multicast a message
      */
     public void cast(String messagetext) {
-        for(int i=0; i < hosts; i++) {
-            /* Sends to everyone except itself */
-            if(i != id) {
-                bcom.basicsend(i,new ExampleMessage(id, messagetext));
-            }
+        if (id == sequencerId){
+            //sequencer
+            sequencerCast(messagetext, id);
+            mcui.debug("Sent out: \""+messagetext+"\"");
+            mcui.deliver(id, messagetext, "from myself!");
+        
+        } else {
+            //node is not a sequencer, send to sequencer
+            bcom.basicsend(sequencerId, new MyMessage(id, messagetext, id, 0));
+            mcui.debug("Send out to sequencer");
         }
-        mcui.debug("Sent out: \""+messagetext+"\"");
-        mcui.deliver(id, messagetext, "from myself!");
     }
     
     /**
@@ -39,7 +72,16 @@ public class ReliableOrderedCaster extends Multicaster {
      * @param message  The message received
      */
     public void basicreceive(int peer,Message message) {
-        mcui.deliver(peer, ((ExampleMessage)message).text);
+        if (id == sequencerId){
+            //sequencer, 
+            sequencerCast(((MyMessage)message).text, ((MyMessage)message).messageCreator);
+            mcui.debug("Sent out: \""+((MyMessage)message).text+"\"");
+            mcui.deliver(((MyMessage)message).messageCreator, ((MyMessage)message).text, "from myself!");
+
+        } else{
+            //not a sequencer, wait until localSequence == S, then deliver a message
+            mcui.deliver(((MyMessage)message).messageCreator, ((MyMessage)message).text);
+        }
     }
 
     /**
--- start_client.sh
+++ start_client.sh
@@ -1,3 +1,7 @@
 java mcgui.Main ReliableOrderedCaster 0 localhostsetup&
 java mcgui.Main ReliableOrderedCaster 1 localhostsetup&
 java mcgui.Main ReliableOrderedCaster 2 localhostsetup&
+
+#java mcgui.Main ExampleCaster 0 localhostsetup&
+#java mcgui.Main ExampleCaster 1 localhostsetup&
+#java mcgui.Main ExampleCaster 2 localhostsetup&
